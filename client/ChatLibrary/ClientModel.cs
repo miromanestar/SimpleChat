5c4e7044abf963fcabf45c86991bc6640c251444
Miro Manestar <miroimanestar@gmail.com>
2020-09-13 14:06:21 -0400

    Fixed client project strucutre

---
+++ client/ChatLibrary/ClientModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+
+namespace ChatLibrary {
+    public class ClientModel : INotifyPropertyChanged
+    {
+        private TcpClient socket;
+        private NetworkStream stream;
+
+        private string messageBoard;
+        public string MessageBoard {
+            get { return messageBoard; }
+            set { messageBoard = value; OnPropertyChanged("MessageBoard"); }
+        }
+
+        private string currentMessage;
+        public string CurrentMessage {
+            get { return currentMessage; }
+            set { currentMessage = value; OnPropertyChanged("CurrentMessage"); }
+        }
+
+        private bool connected;
+        public bool Connected {
+            get { return connected; }
+            set { connected = value; OnPropertyChanged("Connected"); }
+        }
+
+        public ClientModel() {
+            connected = false;
+        }
+
+        #region INotifyPropertyChanged Code
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
